c240f18d34da8551845048ec641b187ae65b909e
Pedro Araujo <pa8088253@gmail.com>
2025-02-21 23:10:27 -0300

    Primeiro commit código

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public abstract class Veiculo
+{
+
+    protected double TaxaDiaria { get; set; }
+
+    
+    public abstract double CalcularValorTotal(int numDiarias);
+}
+
+public class Carro : Veiculo
+{
+   
+    public Carro()
+    {
+        TaxaDiaria = 50.0;
+    }
+
+  
+    public override double CalcularValorTotal(int numDiarias)
+    {
+        return TaxaDiaria * numDiarias; 
+    }
+}
+public class Moto : Veiculo
+{
+    public Moto()
+    {
+        TaxaDiaria = 25.0;
+    }
+
+    public override double CalcularValorTotal(int numDiarias)
+    {
+        return TaxaDiaria * numDiarias;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro meuCarro = new Carro();
+        double valorCarro = meuCarro.CalcularValorTotal(10);
+        Console.WriteLine($"Valor total da locação do carro para 10 diárias: R$ {valorCarro}");
+
+        
+        Moto minhaMoto = new Moto();
+        double valorMoto = minhaMoto.CalcularValorTotal(10);
+        Console.WriteLine($"Valor total da locação da moto para 10 diárias: R$ {valorMoto}");
+    }
+}
